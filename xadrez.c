2cdbb833347f7034448f888fc918e9fa768e2b46
Rafael-itl <dsrafael743@gmail.com>
2025-04-03 20:54:11 +0000

    desafio intermediario completo

--- xadrez.c
+++ xadrez.c
@@ -3,6 +3,8 @@
 #define MOV_BISPO 5
 #define MOV_TORRE 5
 #define MOV_RAINHA 8
+#define MOV_CAVALO_LINHAS 2
+#define MOV_CAVALO_COLUNAS 1
 
 int main() {
     printf("\nmovimentação do bispo:\n");
@@ -23,6 +25,16 @@ int main() {
         count++;
      }
 
+     printf("\nmovimentação do cavalo\,");
+     for(int i = 0; i < MOV_CAVALO_LINHAS; i++) {
+        printf("baixo\n");
+     }
+
+     int j = 0;
+     while (j <  MOV_CAVALO_COLUNAS) {
+        printf("esquerda\n");
+        j++;
+     }
 
     return 0;
 }
